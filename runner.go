56a2590d96c19ed3f9bf13c0e3ce7a68fef74cb7
Sotiris Poulias <str.poulias@gmail.com>
2018-09-07 16:41:09 +0300

    Cleanup mutexes and fixed data races

--- runner.go
+++ runner.go
@@ -7,25 +7,22 @@ import (
 	"time"
 )
 
-// Runner orchestrates and handles jobs
+// Runner orchestrates and handles jobs.
 type Runner struct {
 	jobs     []*job
-	errors   []chan error
 	callback func() error
-	quit     chan bool
-	mu       *sync.Mutex
+	quit     chan struct{}
 }
 
-// job defines a channel to run on something
+// job defines a channel to run on something.
 type job struct {
 	id       int
 	interval time.Duration
-	quit     chan bool
+	quit     chan struct{}
 	callback func() error
-	mu       *sync.Mutex
 }
 
-// New creates a runner with its jobs
+// New creates a runner with its jobs.
 func New(count int, interval time.Duration, callback func() error) *Runner {
 	jobs := make([]*job, count)
 	for i := 0; i < count; i++ {
@@ -33,28 +30,30 @@ func New(count int, interval time.Duration, callback func() error) *Runner {
 		jobs[i] = job
 	}
 	return &Runner{
-		jobs:   jobs,
-		errors: make([]chan error, count),
-		quit:   make(chan bool),
-		mu:     &sync.Mutex{},
+		jobs: jobs,
+		quit: make(chan struct{}),
 	}
 }
 
-// Start executes all the jobs declared
+// Start executes all the jobs declared.
 func (r *Runner) Start() {
-	wg := &sync.WaitGroup{}
-	for _, j := range r.jobs {
-		fmt.Printf("job %d starting\n", j.id)
-		go j.execute(wg)
-	}
+	go func() {
+		wg := &sync.WaitGroup{}
+		for _, j := range r.jobs {
+			wg.Add(1)
+			fmt.Printf("job %d starting\n", j.id)
+			go func(j *job, wg *sync.WaitGroup) {
+				j.execute()
+				wg.Done()
+			}(j, wg)
+		}
 
-	wg.Wait()
+		wg.Wait()
+	}()
 }
 
-// Stop gracefully sends the quit signal to jobs
+// Stop gracefully sends the quit signal to jobs.
 func (r *Runner) Stop() {
-	r.mu.Lock()
-	defer r.mu.Unlock()
 	for _, j := range r.jobs {
 		j.stop()
 	}
@@ -64,15 +63,12 @@ func newJob(id int, interval time.Duration, callback func() error) *job {
 	return &job{
 		id:       id,
 		interval: interval,
-		quit:     make(chan bool),
+		quit:     make(chan struct{}),
 		callback: callback,
-		mu:       &sync.Mutex{},
 	}
 }
 
-func (j *job) execute(wg *sync.WaitGroup) {
-	wg.Add(1)
-	defer wg.Done()
+func (j *job) execute() {
 	for {
 		err := j.callback()
 		if err != nil {
@@ -88,8 +84,6 @@ func (j *job) execute(wg *sync.WaitGroup) {
 }
 
 func (j *job) stop() {
-	j.mu.Lock()
-	defer j.mu.Unlock()
 	fmt.Printf("job %d clossing\n", j.id)
-	j.quit <- true
+	close(j.quit)
 }
